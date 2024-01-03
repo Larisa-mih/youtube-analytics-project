a151cb865decf3feb43a01e8a46062ac914a648c
larisa <larisa.mih2004@gmail.com>
2024-01-04 01:55:02 +0300

    commented out line 12 for homework 2

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,15 +1,15 @@
 from src.channel import Channel
 
 if __name__ == '__main__':
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    moscowpython = Channel("UC-OVMPlMA3-YCIeg4z5z23A")
 
     # получаем значения атрибутов
     print(moscowpython.title)  # MoscowPython
-    print(moscowpython.video_count)  # 685 (может уже больше)
+    print(moscowpython.videoCount)  # 685 (может уже больше)
     print(moscowpython.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
 
     # менять не можем
-    moscowpython.channel_id = 'Новое название'
+    # moscowpython.channel_id = 'Новое название'
     # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
     # можем получить объект для работы с API вне класса
